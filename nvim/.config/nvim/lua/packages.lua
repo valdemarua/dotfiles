5e401b3236dc4dffc7ad02e6a1cda6a77e4de8d7
valdemarua <valdemarua@users.noreply.github.com>
2022-10-17 15:48:34 +0300

    Add ruby plugins to nvim

--- nvim/.config/nvim/lua/packages.lua
+++ nvim/.config/nvim/lua/packages.lua
@@ -7,6 +7,11 @@ return require('packer').startup(function(use)
     run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
   }
 
+  -- ruby, rails, rspec
+  use 'tpope/vim-rails'
+  use 'vim-ruby/vim-ruby'
+  use 'keith/rspec.vim'
+
   -- color schemes
   use 'folke/tokyonight.nvim'
 end)
