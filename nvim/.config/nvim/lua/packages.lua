fd9f6dff53a8314e8d6f35f30ae86301ed1b78ca
valdemarua <valdemarua@users.noreply.github.com>
2022-12-26 11:37:42 +0200

    Add autopairs plugin for nvim

--- nvim/.config/nvim/lua/packages.lua
+++ nvim/.config/nvim/lua/packages.lua
@@ -71,6 +71,12 @@ return require('packer').startup(function(use)
       require('gitsigns').setup()
     end
   }
+
+  -- Autopairs
+  use {
+    "windwp/nvim-autopairs",
+    config = function() require("nvim-autopairs").setup {} end
+  }
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if packer_bootstrap then
