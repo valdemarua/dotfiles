2d8429296df21f8137b147d0e1984cf0f234f1ba
valdemarua <valdemarua@users.noreply.github.com>
2023-03-19 22:04:14 +0200

    nvim: add cmp-cmdline plugin

--- nvim/.config/nvim/lua/lazynvim.lua
+++ nvim/.config/nvim/lua/lazynvim.lua
@@ -31,11 +31,12 @@ require("lazy").setup({
   -- CMP
   {'hrsh7th/nvim-cmp',
     dependencies = {
-      'hrsh7th/cmp-nvim-lsp',
-      'hrsh7th/cmp-buffer',
-      'hrsh7th/cmp-path',
-      'hrsh7th/cmp-nvim-lua',
       'L3MON4D3/LuaSnip',
+      'hrsh7th/cmp-buffer',
+      'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-nvim-lua',
+      'hrsh7th/cmp-path',
+      'hrsh7th/cmp-cmdline',
       'saadparwaiz1/cmp_luasnip'
     },
   },
