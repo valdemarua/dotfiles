d095879f681ee1a616e706ec195314dde94b5eb0
valdemarua <valdemarua@users.noreply.github.com>
2023-03-25 00:12:03 +0200

    nvim: add some new mappings

--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -1,54 +1,57 @@
 local function map(mode, lhs, rhs, opts)
-  local options = { noremap = true, silent = true }
-  if opts then
-    options = vim.tbl_extend('force', options, opts)
-  end
-  vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+	local options = { noremap = true, silent = true }
+	if opts then
+		options = vim.tbl_extend("force", options, opts)
+	end
+	vim.api.nvim_set_keymap(mode, lhs, rhs, options)
 end
 
 -- Change leader to a space
-vim.g.mapleader = ' '
+vim.g.mapleader = " "
 
 -- Clear search highlighting with <leader> and c
-map('n', '<leader>c', ':nohl<CR>')
+map("n", "<leader>c", ":nohl<CR>")
 
 -- Reload configuration without restart nvim
-map('n', '<leader>r', ':so %<CR>')
+map("n", "<leader>r", ":so %<CR>")
 -- Open nvim config
-map('n', '<Leader>rc', ':vs ~/.config/nvim/init.lua<CR>')
+map("n", "<Leader>rc", ":vs ~/.config/nvim/init.lua<CR>")
 
 -- Close buffer with <leader> and q
-map('n', '<leader>q', '<c-w>q')
+map("n", "<leader>q", "<c-w>q")
+map("n", "<leader>w", ":w<CR>")
 
 -- Save buffer with Ctrl+s
-map('n', '<c-s>', ':w<CR>', {})
-map('i', '<c-s>', '<Esc>:w<CR>a', {})
+map("n", "<c-s>", ":w<CR>", {})
+map("i", "<c-s>", "<Esc>:w<CR>a", {})
+
+-- Add new line by with Enter in normal mode
+map("n", "<CR>", "o<Esc>", {})
 
 -- nvim-tree
-map('n', '<c-n>', ':NvimTreeToggle<CR>', {})
-map('n', '<c-f>', ':NvimTreeFindFile<CR>', {})
+map("n", "<c-n>", ":NvimTreeToggle<CR>", {})
+map("n", "<c-f>", ":NvimTreeFindFile<CR>", {})
 
 -- Move around splits using Ctrl + {h, j, k, l}
 local opts = { noremap = true }
-map('n', '<c-h>', '<c-w>h', opts)
-map('n', '<c-j>', '<c-w>j', opts)
-map('n', '<c-k>', '<c-w>k', opts)
-map('n', '<c-l>', '<c-w>l', opts)
+map("n", "<c-h>", "<c-w>h", opts)
+map("n", "<c-j>", "<c-w>j", opts)
+map("n", "<c-k>", "<c-w>k", opts)
+map("n", "<c-l>", "<c-w>l", opts)
 
 -- Telescope
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>p', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<leader>p", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
 
 -- Alternate Toggler
-vim.keymap.set("n", "+", "<cmd>lua require('alternate-toggler').toggleAlternate()<CR>"
-)
+vim.keymap.set("n", "+", "<cmd>lua require('alternate-toggler').toggleAlternate()<CR>")
 
 -- Vim test
-vim.keymap.set('n', '<leader>t', ':TestNearest<CR>', {})
-vim.keymap.set('n', '<leader>T', ':TestFile<CR>', {})
-vim.keymap.set('n', '<leader>a', ':TestSuite<CR>', {})
-vim.keymap.set('n', '<leader>l', ':TestLast<CR>', {})
-vim.keymap.set('n', '<leader>g', ':TestVisit<CR>', {})
+vim.keymap.set("n", "<leader>t", ":TestNearest<CR>", {})
+vim.keymap.set("n", "<leader>T", ":TestFile<CR>", {})
+vim.keymap.set("n", "<leader>a", ":TestSuite<CR>", {})
+vim.keymap.set("n", "<leader>l", ":TestLast<CR>", {})
+vim.keymap.set("n", "<leader>g", ":TestVisit<CR>", {})
