6a8f625cfcaa5c9a7b01f17d133b187601322080
valdemarua <valdemarua@users.noreply.github.com>
2023-11-10 16:22:28 +0200

    mac: add settings for finder and screenshots location

--- mac-preferences.sh
+++ mac-preferences.sh
@@ -3,7 +3,22 @@
 # Close System Preferences
 osascript -e 'tell application "System Preferences" to quit'
 
+# Increase window resize speed for Cocoa applications
+defaults write NSGlobalDomain NSWindowResizeTime -float 0.001
+
+################################################
+# Finder
+################################################
+
+# Finder: disable window animations and Get Info animations
+defaults write com.apple.finder DisableAllAnimations -bool true
+
+# Show status bar
+defaults write com.apple.finder ShowStatusBar -bool true
+
+################################################
 # Dock
+################################################
 
 # Hide Dock
 defaults write com.apple.dock autohide -bool true # && killall Dock
@@ -61,6 +76,13 @@ defaults write com.apple.dock wvous-tr-corner -int 4
 # Set Modifiers
 defaults write com.apple.dock wvous-tl-modifier -int 1048576
 defaults write com.apple.dock wvous-tr-modifier -int 1048576
+
+################################################
+# Screenshots
+################################################
+
+mkdir -p ~/Documents/Screenshots
+defaults write com.apple.screencapture "location" -string "~/Documents/Screenshots"
 
 # Restart the Dock
 killall Dock
