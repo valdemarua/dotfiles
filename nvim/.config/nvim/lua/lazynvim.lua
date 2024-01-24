61561b6afd1a18f8704a179b8001c09c7b2513cb
valdemarua <valdemarua@users.noreply.github.com>
2024-01-24 19:45:38 +0200

    nvim: some startup improvements

--- nvim/.config/nvim/lua/lazynvim.lua
+++ nvim/.config/nvim/lua/lazynvim.lua
@@ -72,6 +72,7 @@ require("lazy").setup({
   {
     "kylechui/nvim-surround",
     version = "*",
+    event = "VeryLazy",
     config = function()
       require("nvim-surround").setup()
     end,
@@ -86,9 +87,8 @@ require("lazy").setup({
   },
   {
     "windwp/nvim-autopairs",
-    config = function()
-      require("nvim-autopairs").setup()
-    end,
+    event = "InsertEnter",
+    opts = {}, -- this is equalent to setup({}) function
   },
   -- Want to turn fooBar into foo_bar? Press crs (coerce to snake_case).
   -- MixedCase (crm), camelCase (crc), snake_case (crs), UPPER_CASE (cru),
@@ -125,20 +125,8 @@ require("lazy").setup({
       { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end,       desc = "Flash" },
       { "S", mode = { "n", "o", "x" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
       { "r", mode = "o",               function() require("flash").remote() end,     desc = "Remote Flash" },
-      {
-        "R",
-        mode = { "o", "x" },
-        function() require("flash").treesitter_search() end,
-        desc =
-        "Treesitter Search"
-      },
-      {
-        "<c-s>",
-        mode = { "c" },
-        function() require("flash").toggle() end,
-        desc =
-        "Toggle Flash Search"
-      },
+      { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+      { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
     },
     {
       "NvChad/nvim-colorizer.lua",
