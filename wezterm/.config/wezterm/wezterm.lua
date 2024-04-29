bbe1d167e9d5b59619bae7840969aa9cca9fd223
valdemarua <valdemarua@users.noreply.github.com>
2024-04-29 23:26:04 +0300

    wezterm: add new font, use dynamic font size

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -21,19 +21,25 @@ config.window_decorations = "RESIZE"
 config.window_close_confirmation = "NeverPrompt" -- 'AlwaysPrompt'
 config.use_dead_keys = false
 
-config.font_size = 14.0
-
 -- config.font = wezterm.font "Noto Color Emoji"
 -- config.font = wezterm.font("Fira Code")
 -- config.font = wezterm.font("JetBrains Mono")
-config.font = wezterm.font("Rec Mono Linear")
+config.font = wezterm.font("Fantasque Sans Mono")
+-- config.font = wezterm.font("Rec Mono Linear")
 -- config.font = wezterm.font("Source Code Pro")
 -- config.font = wezterm.font("Hack")
 -- config.font = wezterm.font("0xProto Nerd Font")
 
+config.font_size = 14.0
+
+if config.font.font[1].family == "Fantasque Sans Mono" then
+  config.font_size = 16.0
+end
+
 config.use_fancy_tab_bar = false
 config.tab_bar_at_bottom = false
 config.hide_tab_bar_if_only_one_tab = true
+config.adjust_window_size_when_changing_font_size = false
 
 config.window_padding = {
   left = 2,
@@ -51,7 +57,12 @@ config.window_padding = {
 --       italic = false,
 --     },
 --   },
+--
 -- }
+
+-- FOR DEBUG
+-- You can find log files in $XDG_RUNTIME_DIR/wezterm on unix systems, or $HOME/.local/share/wezterm on macOS and Windows systems.
+-- wezterm.log_info(config)
 
 -- and finally, return the configuration to wezterm
 return config
