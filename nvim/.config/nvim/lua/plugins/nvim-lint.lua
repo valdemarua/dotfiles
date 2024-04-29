0225ea9112a24d91efc3aef253f85e9b528c2d4c
valdemarua <valdemarua@users.noreply.github.com>
2024-04-29 23:25:46 +0300

    nvim: Add biomejs to nvim-lint

--- nvim/.config/nvim/lua/plugins/nvim-lint.lua
+++ nvim/.config/nvim/lua/plugins/nvim-lint.lua
@@ -3,6 +3,10 @@ local lint = require("lint")
 lint.linters_by_ft = {
   -- markdown = { "vale" },
   ruby = { "rubocop" },
+  javascript = { "biomejs" },
+  typescript = { "biomejs" },
+  javasriptreact = { "biomejs" },
+  typescriptreact = { "biomejs" },
 }
 
 local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
