a39d9442de27038d13243e12e23cff00955af0a0
valdemarua <valdemarua@users.noreply.github.com>
2024-05-15 00:21:00 +0300

    nvim: add todo, note, fix snippets

--- nvim/.config/nvim/luasnippets/all.lua
+++ nvim/.config/nvim/luasnippets/all.lua
@@ -1,3 +1,77 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local sn = ls.snippet_node
+local isn = ls.indent_snippet_node
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local c = ls.choice_node
+local d = ls.dynamic_node
+local r = ls.restore_node
+local events = require("luasnip.util.events")
+local ai = require("luasnip.nodes.absolute_indexer")
+local extras = require("luasnip.extras")
+local l = extras.lambda
+local rep = extras.rep
+local p = extras.partial
+local m = extras.match
+local n = extras.nonempty
+local dl = extras.dynamic_lambda
+local fmt = require("luasnip.extras.fmt").fmt
+local fmta = require("luasnip.extras.fmt").fmta
+local conds = require("luasnip.extras.expand_conditions")
+local postfix = require("luasnip.extras.postfix").postfix
+local types = require("luasnip.util.types")
+local parse = require("luasnip.util.parser").parse_snippet
+local ms = ls.multi_snippet
+local k = require("luasnip.nodes.key_indexer").new_key
+
 return {
-  s("trig", t("loaded!!"))
+  s("trig", t("loaded!!")),
+  s(
+    { trig = "todo", desc = "Adds TODO comment", docstring = "// TODO: |" },
+    fmt(
+      [[
+    {} TODO: {}
+  ]],
+      {
+        f(function(_, parent, _)
+          local env = parent.snippet.env
+          return env.LINE_COMMENT
+        end),
+        i(0, ""),
+      }
+    )
+  ),
+  s(
+    "note",
+    fmt(
+      [[
+    {} NOTE: {}
+  ]],
+      {
+        f(function(_, parent, _)
+          local env = parent.snippet.env
+          return env.LINE_COMMENT
+        end),
+        i(0, ""),
+      }
+    )
+  ),
+  s(
+    "fix",
+    fmt(
+      [[
+    {} {}: {}
+  ]],
+      {
+        f(function(_, parent, _)
+          local env = parent.snippet.env
+          return env.LINE_COMMENT
+        end),
+        c(1, { i(0, "FIX"), i(0, "FIXIT") }),
+        i(2, ""),
+      }
+    )
+  ),
 }
