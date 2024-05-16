d4b7ed60e158c9196b07a4183cfcbc09c0717119
valdemarua <valdemarua@users.noreply.github.com>
2024-05-16 12:03:13 +0300

    wezterm: add ability to change panes via keymaps

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,5 +1,6 @@
 -- Pull in the wezterm API
 local wezterm = require("wezterm")
+local act = wezterm.action
 
 -- This table will hold the configuration.
 local config = {}
@@ -28,6 +29,24 @@ config.keys = {
     key = "m",
     mods = "CMD",
     action = wezterm.action.DisableDefaultAssignment,
+  },
+  -- activate pane selection mode with the default alphabet (labels are "a", "s", "d", "f" and so on)
+  { key = "8", mods = "CTRL", action = act.PaneSelect },
+  -- activate pane selection mode with numeric labels
+  {
+    key = "9",
+    mods = "CTRL",
+    action = act.PaneSelect({
+      alphabet = "1234567890",
+    }),
+  },
+  -- show the pane selection mode, but have it swap the active and selected panes
+  {
+    key = "0",
+    mods = "CTRL",
+    action = act.PaneSelect({
+      mode = "SwapWithActive",
+    }),
   },
 }
 
