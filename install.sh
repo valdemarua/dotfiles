ddc4280ba5b4386f69de1bf7db65914d3dc6464c
valdemarua <valdemarua@users.noreply.github.com>
2024-06-18 00:11:09 +0300

    Add ChatGPT mac app

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 mas install 1475387142 # Tailscale
 mas install 1278508951 # Trello
 mas install 540348655  # Monosnap
+mas install 6448311069 # ChatGPT
 
 # Install Lua tools
 luarocks install luacheck
