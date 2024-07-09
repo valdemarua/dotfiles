1d9153a6c2b641c2a3780737934d8c3717acfc97
valdemarua <valdemarua@users.noreply.github.com>
2024-07-09 13:32:02 +0300

    Mac: add toggl app

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 mas install 1475387142 # Tailscale
 mas install 1278508951 # Trello
 mas install 540348655  # Monosnap
+mas install 1291898086 # Toggl
 
 # Install Lua tools
 luarocks install luacheck
