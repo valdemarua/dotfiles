f1e2ecf1a670b0193c91bbd975462a536f188948
valdemarua <valdemarua@users.noreply.github.com>
2025-07-23 15:27:59 +0300

    nvim: use sonnet 3.7 for avante

--- nvim/.config/nvim/lua/plugins/avante.lua
+++ nvim/.config/nvim/lua/plugins/avante.lua
@@ -5,7 +5,8 @@ require("avante_lib").load()({
   providers = {
     claude = {
       endpoint = "https://api.anthropic.com",
-      model = "claude-sonnet-4-20250514",
+      -- model = "claude-sonnet-4-20250514",
+      model = "claude-3-7-sonnet-20250219",
       timeout = 30000, -- Timeout in milliseconds
       extra_request_body = {
         temperature = 0.75,
