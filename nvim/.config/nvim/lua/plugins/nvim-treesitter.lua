28220305f89f0e82d9166515a66b11c513a94896
valdemarua <valdemarua@users.noreply.github.com>
2026-05-05 14:51:56 +0300

    nvim: enable treesitter highlight

--- nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -44,3 +44,12 @@ vim.api.nvim_create_autocmd("FileType", {
   pattern = "eruby.yaml",
   command = "set filetype=yaml",
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  callback = function()
+    -- Enable treesitter highlighting and disable regex syntax
+    pcall(vim.treesitter.start)
+    -- Enable treesitter-based indentation
+    vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+  end,
+})
